f4126a6aabe3f4ed00ed1d12b049f008472f2fd7
Salil Khanna <salilkhannawork@gmail.com>
2023-04-18 22:13:33 -0400

    for vercel

--- app.js
+++ app.js
@@ -10,8 +10,11 @@ mongoose.connect(CONNECTION_STRING);
 const app = express()
 app.use(cors())
 app.use(express.json());
-TuitsController(app);
-HelloController(app)
-UserController(app)
+app.get('/', (req, res) => {
+    res.send('Welcome to Full Stack Development!')
+})
+// TuitsController(app);
+// HelloController(app)
+// UserController(app)
 app.listen(process.env.PORT || 4000);
 module.exports = app;
