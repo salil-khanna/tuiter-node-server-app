f1f915219522164fb1ba3f38796722d9bb8ec26f
Salil Khanna <salilkhannawork@gmail.com>
2023-04-18 23:04:45 -0400

    firebase changes

---
+++ functions/index.js
@@ -0,0 +1,23 @@
+const functions = require("firebase-functions");
+
+const express = require('express');
+const cors = require('cors');
+const HelloController = require('./controllers/hello-controller');
+const UserController = require('./controllers/users/users-controller');
+const TuitsController = require('./controllers/tuits/tuits-controller');
+const mongoose = require('mongoose');
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/tuiter';
+mongoose.connect(CONNECTION_STRING);
+const app = express()
+app.use(cors())
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+// TuitsController(app);
+HelloController(app)
+// UserController(app)
+app.listen(process.env.PORT || 4000);
+exports.app = functions.https.onRequest(app);
